cef37cb5f47b6cd0e4a5351148c3ae0c92e32c1b
Ian Fisher <iafisher@protonmail.com>
2018-10-18 11:12:02 -0400

    Add init, push and pull commands, which do nothing

--- drive/main.py
+++ drive/main.py
@@ -1,2 +1,23 @@
+"""The entry point into the `drive` executable. Parses command-line arguments
+and then dispatches to the proper function in sync.py.
+
+Author:  Ian Fisher (iafisher@protonmail.com)
+Version: October 2018
+"""
+import argparse
+
+from .sync import init, pull, push
+
+
 def main():
-    print('Hello, world!')
+    parser = argparse.ArgumentParser(description='Sync Google Drive files')
+    parser.add_argument('action', choices=['init', 'push', 'pull'])
+    parser.add_argument('-d', '--dir', default='.')
+
+    args = parser.parse_args()
+    if args.action == 'init':
+        init()
+    elif args.action == 'pull':
+        pull(args.dir)
+    else:
+        pull(args.dir)
---
+++ drive/sync.py
@@ -0,0 +1,17 @@
+"""The main logic of the program: pushing and pulling files from Google Drive.
+
+Author:  Ian Fisher (iafisher@protonmail.com)
+Version: October 2018
+"""
+
+
+def init():
+    pass
+
+
+def push(directory):
+    pass
+
+
+def pull(directory):
+    pass
